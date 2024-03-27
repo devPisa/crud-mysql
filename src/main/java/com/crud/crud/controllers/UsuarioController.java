e2ca72ffce44e97129499d3aec549792043675c9
Cesar Augusto da Silva Pisa <cesar.pisa@alunos.bpkedu.com.br>
2024-03-27 20:17:22 -0300

    Adicionado Front

--- src/main/java/com/crud/crud/controllers/UsuarioController.java
+++ src/main/java/com/crud/crud/controllers/UsuarioController.java
@@ -4,7 +4,6 @@ import com.crud.crud.domain.users.Usuario;
 import com.crud.crud.domain.users.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -12,21 +11,34 @@ import java.util.List;
 public class UsuarioController {
     @Autowired
     private UsuarioRepository usuarioRepository;
-
+    @GetMapping("/assessment")
+    public String index(){
+        return "index";
+    }
     @GetMapping
     public List<Usuario> listarUsuario(){
+
         return usuarioRepository.findAll();
     }
+    @CrossOrigin(origins = "http://localhost:8080")
     @PostMapping
     public Usuario criarUsuario(@RequestBody Usuario usuario){
+
         return usuarioRepository.save(usuario);
     }
     @PutMapping("/{id}")
     public Usuario atualizarUsuario
             (@PathVariable Long id, @RequestBody Usuario usuarioAtualizado){
-        return null;
+        return usuarioRepository.findById(id)
+                .map(usuario -> {
+                    usuario.setNome(usuarioAtualizado.getNome());
+                    usuario.setEmail(usuarioAtualizado.getEmail());
+                    return usuarioRepository.save(usuario);
+                })
+                .orElseThrow();
     }
     @DeleteMapping("/{id}")
     public void deletarUsuario(@PathVariable Long id){
+        usuarioRepository.deleteById(id);
     }
 }
